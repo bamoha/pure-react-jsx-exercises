a29f55953c3b805951f51f66877ad61d0fae2c68
Bash <bashgrey@greymatecare.com>
2019-08-27 14:04:55 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// function SayHello() {
+//     return (
+//         <div className='book' >
+//             <div className='title' >
+//                 The Title
+//             </div>
+//             <div className='author' >
+//                 The Author
+//             </div>
+//             <ul className='stats' >
+//                 <li className='rating' >
+//                     5 stars
+//             </li>
+//                 <li className='isbn' >
+//                     12-345678-910
+//             </li>
+//             </ul>
+//         </div>
+//     )
+// }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Greetings() {
+    let username = "root";
+    // let username = undefined;
+    // let username = null;
+    // let username = false;
+
+    return (
+        <div className='book' >
+            <div className='title' >
+                Hello {username}
+            </div>
+            <div className='author' >
+                The Author
+            </div>
+            <ul className='stats' >
+                <li className='rating' >
+                    5 stars
+            </li>
+                <li className='isbn' >
+                    12-345678-910
+            </li>
+            </ul>
+        </div>
+    )
+}
+
+ReactDom.render(<Greetings />, document.querySelector('#root'))
